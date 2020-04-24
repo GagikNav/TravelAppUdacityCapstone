3c23e5d93ebd801c9801ce775e54692553435310
Gagik <gagik.n@gmail.com>
2020-04-24 18:13:40 +0200

    Chore : get weatherData added

--- src/client/js/getGeonames.js
+++ src/client/js/getGeonames.js
@@ -1,38 +1,41 @@
-const apiID = 'gagdev';
 const cityName = 'london';
 const countryCode = 'NL';
 
-const baseURL = `http://api.geonames.org/searchJSON?q=${cityName}&username=${apiID}&maxRows=2`;
+const geoApiID = 'gagdev';
+const GeoURL = `http://api.geonames.org/searchJSON?q=${cityName}&username=${geoApiID}&maxRows=2`;
+
 //Calling chain of  get , post and UI functions
 /*
 getData(baseURL)
-   .then((data) => {
-      console.log(data);
-      console.log(feelings);
-      postData('/myPostRout', {
-         temperature: data.main.temp,
-         date: data.dt,
-         user_respond: feelings,
-      });
-   })
-   .then(updateUI);
+.then((data) => {
+   console.log(data);
+   console.log(feelings);
+   postData('/myPostRout', {
+      temperature: data.main.temp,
+      date: data.dt,
+      user_respond: feelings,
+   });
+})
+.then(updateUI);
 */
 //get data from Geonames API Function
 //
 let lat = '';
 let lng = '';
-const getData = async (url) => {
+const getGeoData = async (url) => {
    const res = await fetch(url);
    try {
       const data = await res.json();
-      let lat = data.geonames[0].lat;
-      let lng = data.geonames[0].lng;
+      let lat = Number(data.geonames[0].lat).toFixed(2);
+      let lng = Number(data.geonames[0].lng).toFixed(2);
       return { lat, lng };
    } catch (error) {
       console.error('error', error);
    }
 };
-getData(baseURL).then((pos) => console.log(pos.lat));
+
+getGeoData(GeoURL).then((pos) => console.log(pos));
+
 //Post Data to server Function
 //
 /*
