4ea69d04c437db030b01184e3d190e78683738e5
Gagik <gagik.n@gmail.com>
2020-04-26 22:25:54 +0200

    chore: Server get Apis response

--- src/client/js/getPixbay.js
+++ src/client/js/getPixbay.js
@@ -1,6 +1,6 @@
-const pixBayApiKey = '16151623-faff4b778440a2f67b4567d8e';
 const city = 'london';
 const pixBayURL = `https://pixabay.com/api/?key=${pixBayApiKey}&q=${city}&image_type=photo&orientation=horizontal&category=places&editors_choice=true`;
+const pixBayApiKey = '16151623-faff4b778440a2f67b4567d8e';
 
 const getpixBayData = async (url) => {
    const res = await fetch(url);
@@ -12,8 +12,9 @@ const getpixBayData = async (url) => {
    }
 };
 getpixBayData(pixBayURL).then((data) => {
+   console.log(data.hits);
    let imgUrl = data.hits[0].largeImageURL;
-   let x = document.createElement('IMG');
-   x.setAttribute('src', imgUrl);
-   document.body.appendChild(x);
+   let cityImage = document.createElement('IMG');
+   cityImage.setAttribute('src', imgUrl);
+   document.body.appendChild(cityImage);
 });
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,23 @@
 const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
 // const webpackDevMiddleware = require('webpack-dev-middleware');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+const fetch = require('node-fetch');
+let cityName = '';
+let lat = '';
+let lng = '';
+let weather = '';
+let temp = '';
+const pixBayApiKey = process.env.PixBay_Api_Key;
+const weathApiKey = process.env.Weath_Api_Key;
+const geoApiID = process.env.Geoname_API_ID;
+//*****************
+
 //$Variables
 let projectData = {};
 const data = [];
@@ -33,23 +46,86 @@ app.use(cors());
 app.use(express.json());
 
 /* Initialize the main project folder*/
-app.use(express.static('dist'));
+app.use(express.static('./../client/views/'));
 
-const port = process.env.port || 30002;
-/* Spin up the server*/
-const server = app.listen(port, listening);
-function listening() {
-   // console.log(server);
-   // console.log(process.env);
-   console.log(`running on localhost: ${port}`);
-}
-//Get Rout
-app.get('/myGetRout', (req, res) => {
-   res.send(projectData);
-   console.log(projectData);
+/*  server*/
+
+let pData = '';
+app.get('/', (req, res) => {
+   res.sendFile(path.resolve('./src/client/views/index.html'));
+});
+app.post('/', (req, res) => {
+   let cityName = req.body.cityName.toUpperCase();
+   const GeoURL = `http://api.geonames.org/searchJSON?q=${cityName}&username=${geoApiID}&maxRows=2`;
+
+   const getGeoData = async (url) => {
+      const respond = await fetch(url);
+      try {
+         const data = await respond.json();
+         let lat = Number(data.geonames[0].lat).toFixed(2);
+         let lng = Number(data.geonames[0].lng).toFixed(2);
+         return { lat, lng };
+      } catch (error) {
+         console.error('error', error);
+      }
+   };
+   //
+
+   //
+   getGeoData(GeoURL).then((pos) => {
+      //
+      console.log(pos);
+      let imgUrl;
+      let lat = pos.lat;
+      let lng = pos.lng;
+      const currentURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lng}&days=16&key=${weathApiKey}`;
+      async function getCurrentData(url) {
+         //const forecastURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&days=16&key=${weathApiKey}`;
+         const respond = await fetch(url);
+         try {
+            const data = await respond.json();
+
+            return data;
+         } catch (error) {
+            console.error('error', error);
+         }
+      }
+      //
+      const pixBayURL = `https://pixabay.com/api/?key=${pixBayApiKey}&q=${cityName}&image_type=photo&orientation=horizontal&category=places&editors_choice=true`;
+
+      const getpixBayData = async (url) => {
+         const res = await fetch(url);
+         try {
+            const data = await res.json();
+            return data;
+         } catch (error) {
+            console.error('error', error);
+         }
+      };
+      getpixBayData(pixBayURL).then((data) => {
+         console.log(data.hits[0]);
+         let imgUrl = data.hits[0].webformatURL;
+         res.write(`<p><img src= "${imgUrl}"  class="icon" alt=""></p>`);
+      });
+
+      //
+      getCurrentData(currentURL).then((data) => {
+         let pData = data;
+         let wIcon = data.data[0].weather.icon;
+         let iconPath = path.resolve(
+            path.join('./src/client/weatherIcons/', wIcon)
+         );
+         const fileUrl = ` https://www.weatherbit.io/static/img/icons/${wIcon}.png`;
+
+         res.write(
+            `<p>The wether in ${cityName} is ${data.data[0].weather.description} and temperature  is : ${data.data[0].temp} degree</p><p><img src= "${fileUrl}"  class="icon" alt=""></p>`
+         );
+         res.send();
+      });
+   });
 });
 
-// post Rout
-app.post('/myPostRout', (req, res) => {
-   projectData = req.body;
+const port = process.env.port || 30002;
+app.listen(port, () => {
+   console.log(`running on localhost: ${port}.....`);
 });
